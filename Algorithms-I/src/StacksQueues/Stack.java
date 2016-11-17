e2f257e2ac671eb8e9d7f373a69fe64a6ca7dcfc
arashout <arash.out@gmail.com>
2016-11-16 19:16:40 -0800

    Not sure what happend

--- Algorithms-I/src/StacksQueues/Stack.java
+++ Algorithms-I/src/StacksQueues/Stack.java
@@ -9,43 +9,50 @@ public class Stack<Item> {
     private int index;
     private int size;
     private Item[] stack;
-    public Stack(){
+
+    public Stack() {
         size = 1;
         index = -1;
         stack = (Item[]) new Object[size];
     }
-    public void push(Item item){
-        index++;
-        /* TODO:
-        * Need to resize array when index is 1/2 size*/
-        if( index > size / 2){
-            size *= 2;
-            Item[] oldStack = stack.clone();
-            stack = (Item[]) new Object[size];
-            for(int i = 0; i < oldStack.length; i++){
-                stack[i] = oldStack[i];
-            }
-        }
-        stack[index] = item;
-    }
-    public Item pop(){
-        return stack[index--];
-    }
-    public boolean isEmpty(){
-        return index == -1;
-    }
-    public int size(){
-        return index + 1;
-    }
-    public void stringRep(){
-        for(int i = 0; i <= index; i++){
-            StdOut.print(stack[i].toString() + ' ');
-        }
-    }
+
     public static void main(String[] args) {
         Stack<Integer> stack = new Stack<Integer>();
         stack.push(4);
         stack.push(3);
         stack.push(3);
+    }
+
+    public void push(Item item) {
+        index++;
+        /* TODO:
+        * Need to resize array when index is 1/2 size*/
+        if (index > size / 2) {
+            size *= 2;
+            Item[] oldStack = stack.clone();
+            stack = (Item[]) new Object[size];
+            for (int i = 0; i < oldStack.length; i++) {
+                stack[i] = oldStack[i];
+            }
+        }
+        stack[index] = item;
+    }
+
+    public Item pop() {
+        return stack[index--];
+    }
+
+    public boolean isEmpty() {
+        return index == -1;
+    }
+
+    public int size() {
+        return index + 1;
+    }
+
+    public void stringRep() {
+        for (int i = 0; i <= index; i++) {
+            StdOut.print(stack[i].toString() + ' ');
+        }
     }
 }
