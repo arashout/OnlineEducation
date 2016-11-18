d106c7799afa338ac1457a80b09551354be46098
arashout <arash.out@gmail.com>
2016-11-17 21:48:32 -0800

    I was close but I need the iterable iterface stuff so I just copied the first bit of queue from the course libraries

--- Algorithms-I/src/Main.java
+++ Algorithms-I/src/Main.java
@@ -1,3 +1,4 @@
+import edu.princeton.cs.algs4.Queue;
 public class Main {
     public static void main(String[] args) {
 
--- Algorithms-I/src/StacksQueues/Deque.java
+++ Algorithms-I/src/StacksQueues/Deque.java
@@ -1,68 +1,161 @@
 package StacksQueues;
 
-/**
- * Created by arash on 2016-11-16.
- */
-// implements Iterable<Item>
-public class Deque<Item>{
-    private class Node{
-        Item item;
-        Node next;
-    }
-    private int size;
-    private Node first;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
 
-    public Deque(){
-        size = 0;
-        first = new Node();
-    }
-    public boolean isEmpty(){
-        return (size == 0);
-    }
-    public int size(){
-        return size;
-    }
-    public void addFirst(Item item){
-        if(size > 0){
-            Node temp = new Node();
-            temp.item = first.item;
-            temp.next = first.next;
-            first.next = temp;
-        }
-        else{ // Empty Queue case
-            first.next = null;
-        }
-        first.item = item;
-        size ++;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class Deque<Item> implements Iterable<Item> {
+    private Node<Item> first;    // beginning of queue
+    private Node<Item> last;     // end of queue
+    private int n;               // number of elements on queue
+
+    // helper linked list class
+    private static class Node<Item> {
+        private Item item;
+        private Node<Item> next;
     }
 
-    public void addLast(Item item)   {
-        if(size == 0){
-            this.addFirst(item);
-            return;
+    /**
+     * Initializes an empty queue.
+     */
+    public Deque() {
+        first = null;
+        last = null;
+        n = 0;
+    }
+
+    /**
+     * Returns true if this queue is empty.
+     *
+     * @return {@code true} if this queue is empty; {@code false} otherwise
+     */
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    /**
+     * Returns the number of items in this queue.
+     *
+     * @return the number of items in this queue
+     */
+    public int size() {
+        return n;
+    }
+
+    /**
+     * Returns the item least recently added to this queue.
+     *
+     * @return the item least recently added to this queue
+     * @throws NoSuchElementException if this queue is empty
+     */
+    public Item peek() {
+        if (isEmpty()) throw new NoSuchElementException("Deque underflow");
+        return first.item;
+    }
+
+    /**
+     * Adds the item to this queue.
+     *
+     * @param item the item to add
+     */
+    public void addLast(Item item) {
+        Node<Item> oldlast = last;
+        last = new Node<Item>();
+        last.item = item;
+        last.next = null;
+        if (isEmpty()) first = last;
+        else oldlast.next = last;
+        n++;
+    }
+
+    /**
+     * Removes and returns element at the front.
+     *
+     * @return at the front
+     * @throws NoSuchElementException if this queue is empty
+     */
+    public Item removeFirst() {
+        if (isEmpty()) throw new NoSuchElementException("Deque underflow");
+        Item item = first.item;
+        first = first.next;
+        n--;
+        if (isEmpty()) last = null;   // to avoid loitering
+        return item;
+    }
+    /**
+     * Removes and returns element at the end.
+     *
+     * @return at the end
+     * @throws NoSuchElementException if this queue is empty
+     */
+    public Item removeLast(){
+        if (isEmpty()) throw new NoSuchElementException("Deque underflow");
+
+    }
+    /**
+     * Returns a string representation of this queue.
+     *
+     * @return the sequence of items in FIFO order, separated by spaces
+     */
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        for (Item item : this) {
+            s.append(item);
+            s.append(' ');
         }
-        else {
-            Node temp = new Node();
-            temp = first;
-            while(temp.next != null){
-                temp = temp.next;
-            }
-            temp.item = item;
-            temp.next = null;
+        return s.toString();
+    }
+
+    /**
+     * Returns an iterator that iterates over the items in this queue in FIFO order.
+     *
+     * @return an iterator that iterates over the items in this queue in FIFO order
+     */
+    public Iterator<Item> iterator() {
+        return new ListIterator<Item>(first);
+    }
+
+    // an iterator, doesn't implement remove() since it's optional
+    private class ListIterator<Item> implements Iterator<Item> {
+        private Node<Item> current;
+
+        public ListIterator(Node<Item> first) {
+            current = first;
         }
 
-        size++;
+        public boolean hasNext() {
+            return current != null;
+        }
+
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+
+        public Item next() {
+            if (!hasNext()) throw new NoSuchElementException();
+            Item item = current.item;
+            current = current.next;
+            return item;
+        }
     }
-    /*
-    public Item removeFirst()                // remove and return the item from the front
-    public Item removeLast()                 // remove and return the item from the end
-    public Iterator<Item> iterator()         // return an iterator over items in order from front to end
-    */
-    public static void main(String[] args){
-        Deque<Integer> deque = new Deque<Integer>();
-        deque.addFirst(5);
-        deque.addFirst(8);
-        deque.addFirst(9);
-        deque.addLast(10);
-    }   // unit testing
+
+
+    /**
+     * Unit tests the {@code Deque} data type.
+     *
+     * @param args the command-line arguments
+     */
+    public static void main(String[] args) {
+        Deque<String> queue = new Deque<String>();
+        while (!StdIn.isEmpty()) {
+            String item = StdIn.readString();
+            if (!item.equals("-"))
+                queue.addLast(item);
+            else if (!queue.isEmpty())
+                StdOut.print(queue.removeFirst() + " ");
+        }
+        StdOut.println("(" + queue.size() + " left on queue)");
+    }
 }
