00f8702af7383dd67a3e2c89297e672b7896a55c
arashout <arash.out@gmail.com>
2017-02-04 15:03:06 -0800

    Done. The regulariztion at the end was the easiest part

--- Machine_Learning-Andrew_Ng/machine-learning-ex4/ex4/nnCostFunction.m
+++ Machine_Learning-Andrew_Ng/machine-learning-ex4/ex4/nnCostFunction.m
@@ -116,9 +116,9 @@ delta2 = tempDelta2 .* sigmoidGradient(z2);
 Theta2_grad = (1/m)*delta3'*a2;
 Theta1_grad = (1/m)*delta2'*a1;
 
-
-
-
+%Regularized gradient
+Theta2_grad(:,2:end) = Theta2_grad(:,2:end) + lambda/m * Theta2(:,2:end);
+Theta1_grad(:,2:end) = Theta1_grad(:,2:end) + lambda/m * Theta1(:,2:end);
 
 
 
