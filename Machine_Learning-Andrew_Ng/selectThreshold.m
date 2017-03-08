954ca659f411e030a8c62787265854b26af443cc
Arash Outadi <arash.out@gmail.com>
2017-03-08 12:15:31 -0800

    Assignment 8 little more done

--- Machine_Learning-Andrew_Ng/selectThreshold.m
+++ Machine_Learning-Andrew_Ng/selectThreshold.m
@@ -22,9 +22,10 @@ for epsilon = min(pval):stepsize:max(pval)
     %               
     % Note: You can use predictions = (pval < epsilon) to get a binary vector
     %       of 0's and 1's of the outlier predictions
-    tp = sum( (pval > .5) & (yval == 1));
-    fp = sum( (pval > .5) & (yval == 0));
-    fn = sum( (pval <= .5) & (yval == 1));
+    pvalBinary =(pval < epsilon);
+    tp = sum( (pvalBinary == 1) & (yval == 1));
+    fp = sum( (pvalBinary == 1) & (yval == 0));
+    fn = sum( (pvalBinary == 0) & (yval == 1));
     prec = tp/(tp + fp);
     rec = tp/(tp + fn);
     F1 = (2 * prec * rec)/(prec + rec);
